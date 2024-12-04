7b1be81fa713b473926dd5d869ce066967cc8389
prashant <prashanttiwary10@gmail.com>
2024-12-04 18:26:23 +0530

    ManyToMany

---
+++ ManyToMany/Spring-Boot-ManyToMany/src/test/java/com/mylearning/springbootmanytomany/SpringBootManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.springbootmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
